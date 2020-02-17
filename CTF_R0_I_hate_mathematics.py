ed6286a2a8e88174030eede5343cf1e28e340dd4
Shawn <41995051+xephora@users.noreply.github.com>
2020-02-16 21:08:29 -0500

    Add files via upload

---
+++ CTF_R0_I_hate_mathematics.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import requests
+import hashlib
+import urllib2
+import re
+from lxml import html
+import binascii
+
+#Challenge Information
+#https://ringzer0ctf.com/challenges/14
+
+#cookie storage
+session_name = 'PHPSESSID'
+session_value = '<CookieValue>'
+
+#cookie session
+session = requests.Session()
+jar = requests.cookies.RequestsCookieJar()
+jar.set(session_name, session_value)
+session.cookies = jar
+
+#request session
+inspected_webpage = session.get('https://ringzer0ctf.com/challenges/32/')
+
+#pull webcontent
+tree = html.fromstring(inspected_webpage.content)
+
+#scrape everything from class "message"
+export_html = tree.xpath('string(//div[@class="message"])')
+#Grab message of binary data
+
+#removing specific words (WORKS BUT REDUNDANT AND SLOPPY!!)
+export_rbm = re.sub("BEGINMESSAGE", "", export_html)
+export_rbem = re.sub("ENDMESSAGE", "", export_rbm)
+
+#String
+str = export_rbem
+
+#Splitting of string
+splitstr = str.split( )
+
+#grabbing substrings
+x = splitstr[4]
+y = splitstr[6]
+z = splitstr[8]
+
+#Conversion to Integer
+x = int(x)
+y = int(y, base=16)
+z = int(z, 2)
+
+#Calculation
+xyz = x + y - z
+
+updated_webpage = session.get('https://ringzer0ctf.com/challenges/32/'+xyz)
+
+print(updated_webpage)
+
+#Debugging Purposes
+"""
+#String
+str = "2309 + 0x242b - 111111001111 = ?"
+
+#Splitting of string
+splitstr = str.split( )
+
+#confirming array
+print(splitstr)
+print('\n')
+
+#grabbing substrings
+x = splitstr[0]
+y = splitstr[2]
+z = splitstr[4]
+
+#Confirming grabbed substrings
+print('Confirming Substrings')
+print(x)
+print(y)
+print(z)
+print('\n')
+
+#Conversion to Integer
+x = int(x)
+y = int(y, base=16)
+z = int(z, 2)
+
+#Confirming Int values
+print('Int Converted')
+print(x)
+print(y)
+print(z)
+
+#Calculation
+xyz = x + y - z
+
+#Results
+print('\n')
+print('Calculated')
+print(xyz)
+
+"""
